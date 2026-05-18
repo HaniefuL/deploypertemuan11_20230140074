f2d09bc3905e13052fc78aecca86f773831f991f
HaniefuL <haniefulgeofarry@gmail.com>
2026-05-18 12:46:36 +0700

    membuat class SecurityConfig

--- src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
+++ src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.deploy.pertemuan11.security;
 
+import com.deploy.pertemuan11.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 }
